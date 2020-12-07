7fd595f927169abfd460f79507d99d8057b841c5
wo1th <74756565+wo1th@users.noreply.github.com>
2020-12-07 15:53:07 +0800

    Create 120701.（未完成）寻找0的相邻坐标.Find.java

---
+++ 120701.（未完成）寻找0的相邻坐标.Find.java
@@ -0,0 +1,30 @@
+
+public class Find {
+
+	void FindZero(int array2d[][]){
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				int xy=array2d[column][row];
+				if (xy==0) {
+					System.out.println();
+					int up=row+1=0
+				}
+//				if (array2d[y][x]==0) {
+//					System.out.println("R_zero"+"("+column+","+row+")");
+//				}
+//				if (array2d[y][x]==0 && column<=array2d[0].length) {
+//					System.out.println("R_left"+"("+column+","+row+")");
+//				}
+//				if (array2d[y][x]==0 && column<=array2d[0].length) {
+//					System.out.println("R_right"+"("+column+","+row+")");
+//				}
+//				if (array2d[y][x]==0 && row<=array2d.length) {
+//					System.out.println("R_up"+"("+column+","+row+")");
+//				}
+//				if (array2d[y][x]==0 && row<=array2d.length) {
+//					System.out.println("R_down"+"("+column+","+row+")");
+//				}
+			}
+		}
+	}
+}
